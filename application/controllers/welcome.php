5ee8dd5ff8d6154c8d2ac69e3144dab95e0e9c13
aw9223 <aw9223@naver.com>
2015-09-03 12:20:44 +0900

    Update welcome.php

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -4,9 +4,7 @@ class Welcome extends CI_Controller
 {
     function __construct() 
     {
-        parent::__construct();    
-		  
-        $this->load->helper('url');  
+        parent::__construct();
 		
         $this->load->library('theme');  
     }
